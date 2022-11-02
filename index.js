4b4ea4253948fe7553fceb438ee99681da859e0f
Daniel <leinad6ix.z@gmail.com>
2022-11-02 05:52:42 -0700

    started beginning of index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const render = require("./src/htmlRenderer");
+
